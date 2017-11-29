b807d014266fab47ffca04fbc264ce93c8e9b69f
Niloufar Alipour Tehrani <nalipour@cern.ch>
2017-11-29 14:46:37 +0100

    A script which calculates the total number of wires in the drift chamber

---
+++ nbWires.py
@@ -0,0 +1,13 @@
+def fun(nRing):
+    def nbWires(SL):
+        sensWires = nRing * (192 + SL * 48)
+        fieldWires = 5 * sensWires
+        total = sensWires + fieldWires
+        return sensWires, fieldWires, total
+    return nbWires
+
+
+wires = list(map(fun(8), range(14)))
+nSens = sum([x[0] for x in wires])
+nField = sum([x[1] for x in wires])
+nTotal = sum([x[2] for x in wires])
