babd624b4f65251e74c4cde8d11b1773dd883936
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-03-02 16:13:58 +0100

    Plots the hits belonging to different wires in different colors. The wires then can be visually seen

--- Segmentation.py
+++ Segmentation.py
@@ -2,10 +2,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-#%matplotlib inline
+%matplotlib inline
+
 
 DATA_PATH = './data'
-Filename = 'angleScan_layer1.csv'
+Filename = 'angleScan_layer2.csv'
 
 csv_path = os.path.join(DATA_PATH, Filename)
 dataset = pd.read_csv(csv_path)
@@ -18,12 +19,17 @@ dataset.hist(bins=50, figsize=(20, 15))
 dataset['cellId'].value_counts()
 dataset.sort_values(by='cellId', ascending=True)
 
+fig = plt.figure()
+ax = Axes3D(fig)
 
 group = dataset.groupby('cellId')
-
 for cellid in group.groups.keys():
     df = group.get_group(cellid)
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    ax.scatter(df['x'], df['y'], df['z'])
-    fig.savefig('./plots/cellid'+str(cellid)+'.pdf')
+    ax.scatter(df['x'], df['z'], df['y'])  # Detector axis placed correctly
+
+ax.set_xlabel('x')
+ax.set_ylabel('z')
+ax.set_zlabel('y')
+
+
+fig.savefig('./plots/allHits_2.pdf')
