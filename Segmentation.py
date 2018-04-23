93b6318199fe6e3d1cbc2e43ed7b38b839e9a39a
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-04-23 14:57:40 +0200

    an update

--- Segmentation.py
+++ Segmentation.py
@@ -2,11 +2,13 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-%matplotlib inline
+# %matplotlib inline
 
 
 DATA_PATH = './data'
-Filename = 'angleScan_layer2.csv'
+# Filename = 'angleScan_layer1.csv'
+# Filename = 'angleScan_layer2.csv'
+Filename = 'simu_test.csv'
 
 csv_path = os.path.join(DATA_PATH, Filename)
 dataset = pd.read_csv(csv_path)
@@ -14,6 +16,13 @@ dataset = pd.read_csv(csv_path)
 dataset.head()
 dataset.info()
 dataset.describe()
+
+# Plot the raw hits
+figHits = plt.figure()
+ax = Axes3D(figHits)
+ax.scatter(dataset['x'], dataset['z'], dataset['y'])
+
+# Plot hits belonging to the same wire
 dataset.hist(bins=50, figsize=(20, 15))
 
 dataset['cellId'].value_counts()
@@ -27,9 +36,9 @@ for cellid in group.groups.keys():
     df = group.get_group(cellid)
     ax.scatter(df['x'], df['z'], df['y'])  # Detector axis placed correctly
 
-ax.set_xlabel('x')
-ax.set_ylabel('z')
-ax.set_zlabel('y')
+ax.set_xlabel('x [mm]')
+ax.set_ylabel('z [mm]')
+ax.set_zlabel('y [mm]')
 
-
-fig.savefig('./plots/allHits_2.pdf')
+# fig.savefig('./plots/allHits.pdf')
+fig.savefig('./plots/allHits_simu_test.pdf')
--- readData.py
+++ readData.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plot_functions as pf
 
-file = pd.read_csv('data/bcp_output.csv')
+file = pd.read_csv('data/gaussDigi_test.csv')
 pf.plotHisto(file, 'x', 'mm', 100)
+pf.plotHisto(file, 'y', 'mm', 100)
+pf.plotHisto(file, 'z', 'mm', 100)
 pf.plotHisto(file, 'time', 'ns')
